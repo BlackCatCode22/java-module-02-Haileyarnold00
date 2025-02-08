00c9b5a6e5709b1e38860cf24483301cebbdc8ba
Hailey Arnold <0989987@my.scccd.edu>
2025-02-07 20:44:07 -0800

    Add files via upload

---
+++ ReversedStrings.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ReversedStrings {
+    public static void main(String[] args) {
+        System.out.print("\n\n Welcome to my reverse string code!!\n\n");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Say something!");
+        int originalStr = scanner.nextInt();
+
+        System.out.println("You said:  " + originalStr);
+
+
+
+
+
+    }
+
+}
